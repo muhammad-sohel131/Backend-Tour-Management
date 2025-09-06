60e7ce58038a373386ec397af0c33299d2678809
= <sohelf131@gmail.com>
2025-09-07 00:31:20 +0600

    feat: send booking invoice PDF via email

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,4 +1,3 @@
-
 import AppError from "../../errorHelpers/AppError";
 import { BOOKING_STATUS } from "../booking/booking.interface";
 import { Booking } from "../booking/booking.model";
@@ -7,32 +6,36 @@ import { User } from "../user/user.model";
 import { PAYMENT_STATUS } from "./payment.interface";
 import { Payment } from "./payment.model";
 import { sslService } from "../sslCommerz/sslCommerz.service";
+import { generatePdf, IInvoiceData } from "../../utils/invoice";
+import { IUser } from "../user/user.interface";
+import { ITour } from "../tour/tour.interface";
+import { sendEmail } from "../../utils/sendEmail";
 
-const initPayment = async(bookingId: string) => {
-    const payment = await Payment.findOne({booking: bookingId})
-    if(!payment){
-        throw new AppError(404, "Payment not found. you have not book this tour.")
-    }
+const initPayment = async (bookingId: string) => {
+  const payment = await Payment.findOne({ booking: bookingId });
+  if (!payment) {
+    throw new AppError(404, "Payment not found. you have not book this tour.");
+  }
 
-    const booking = await Booking.findById(payment.booking)
-    const user = await User.findById(booking?.user)
+  const booking = await Booking.findById(payment.booking);
+  const user = await User.findById(booking?.user);
 
-    const sslCommerz: IsslCommerz = {
-        address: user?.address as string,
-        email: user?.email as string,
-        amount: payment.amount,
-        name: user?.name as string,
-        phoneNumber: user?.phone as string,
-        transaction: payment.transactionId
-    }
+  const sslCommerz: IsslCommerz = {
+    address: user?.address as string,
+    email: user?.email as string,
+    amount: payment.amount,
+    name: user?.name as string,
+    phoneNumber: user?.phone as string,
+    transaction: payment.transactionId,
+  };
 
-    const sslPayment = await sslService.sslPaymentInit(sslCommerz)
+  const sslPayment = await sslService.sslPaymentInit(sslCommerz);
 
-    return {
-      paymentUrl: sslPayment.GatewayPageURL,
-      booking: booking
-    };
-}
+  return {
+    paymentUrl: sslPayment.GatewayPageURL,
+    booking: booking,
+  };
+};
 const successPayment = async (query: Record<string, string>) => {
   const session = await Booking.startSession();
   session.startTransaction();
@@ -52,7 +55,7 @@ const successPayment = async (query: Record<string, string>) => {
       }
     );
 
-    await Booking.findByIdAndUpdate(
+    const updatedBooking = await Booking.findByIdAndUpdate(
       updatedPayment?.booking,
       {
         status: BOOKING_STATUS.COMPLETE,
@@ -62,8 +65,33 @@ const successPayment = async (query: Record<string, string>) => {
         runValidators: true,
         session,
       }
-    );
+    )
+      .populate("tour", "title")
+      .populate("user", "name", "email");
 
+    const invoiceData: IInvoiceData = {
+      username: (updatedBooking?.user as IUser).name,
+      bookingDate: updatedBooking?.createdAt as Date,
+      guestCount: updatedBooking?.guestCount as number,
+      tourTitle: (updatedBooking?.tour as ITour).title,
+      totalAmount: updatedPayment?.amount as number,
+      transactionId: updatedPayment?.transactionId as string,
+    };
+
+    const pdfBuffer = await generatePdf(invoiceData)
+    await sendEmail({
+      to: (updatedBooking?.user as IUser).email,
+      subject: "Your Booking Invoice",
+      templateName: "invoice",
+      templateData: invoiceData,
+      attachments: [
+        {
+          filename: "invoice.pdf",
+          content: pdfBuffer,
+          contentType: "application/pdf"
+        }
+      ]
+    })
     await session.commitTransaction();
     session.endSession();
 
@@ -169,5 +197,5 @@ export const PaymentServices = {
   successPayment,
   failPayment,
   cancelPayment,
-  initPayment
+  initPayment,
 };
--- src/app/utils/invoice.ts
+++ src/app/utils/invoice.ts
@@ -2,16 +2,18 @@
 import AppError from "../errorHelpers/AppError";
 import PDFDocument from "pdfkit";
 
-export interface InvoiceData {
-    transactionId: string,
-    bookingDate: Date,
-    username: string,
-    tourTitle: string,
-    guestCount: number,
-    totalAmount: number
+export interface IInvoiceData {
+  transactionId: string;
+  bookingDate: Date;
+  username: string;
+  tourTitle: string;
+  guestCount: number;
+  totalAmount: number;
 }
 
-const generatePdf = async (invoiceData: InvoiceData) => {
+export const generatePdf = async (
+  invoiceData: IInvoiceData
+): Promise<Buffer<ArrayBufferLike>> => {
   try {
     return new Promise((resolve, reject) => {
       const doc = new PDFDocument({
@@ -19,26 +21,26 @@ const generatePdf = async (invoiceData: InvoiceData) => {
         margin: 50,
       });
 
-      const buffer: Uint8Array[] = []
-      doc.on("data", (chunk) => buffer.push(chunk))
-      doc.on("end", () => resolve(Buffer.concat(buffer)))
-      doc.on("error", (err) => reject(err))
+      const buffer: Uint8Array[] = [];
+      doc.on("data", (chunk) => buffer.push(chunk));
+      doc.on("end", () => resolve(Buffer.concat(buffer)));
+      doc.on("error", (err) => reject(err));
 
-    //   pdf content
-    doc.fontSize(20).text("Invoice,", {align: "center", })
-    doc.moveDown()
+      //   pdf content
+      doc.fontSize(20).text("Invoice,", { align: "center" });
+      doc.moveDown();
 
-    doc.fontSize(14).text(`TransactionId: ${invoiceData.transactionId}`)
-    doc.text(`Booking Date: ${invoiceData.bookingDate}`)
-    doc.text(`Customer : ${invoiceData.username}`)
-    doc.moveDown()
+      doc.fontSize(14).text(`TransactionId: ${invoiceData.transactionId}`);
+      doc.text(`Booking Date: ${invoiceData.bookingDate}`);
+      doc.text(`Customer : ${invoiceData.username}`);
+      doc.moveDown();
 
-    doc.text(`Tour ${invoiceData.tourTitle}`)
-    doc.text(`Guests : ${invoiceData.guestCount}`)
-    doc.text(`Total Amount: ${invoiceData.totalAmount.toFixed(2)}`)
-    doc.moveDown()
+      doc.text(`Tour ${invoiceData.tourTitle}`);
+      doc.text(`Guests : ${invoiceData.guestCount}`);
+      doc.text(`Total Amount: ${invoiceData.totalAmount.toFixed(2)}`);
+      doc.moveDown();
 
-    doc.text(`Thank you for booking with us!`, {align: "center"})
+      doc.text(`Thank you for booking with us!`, { align: "center" });
     });
   } catch (error: any) {
     console.log(`Invoice Error, ${error}`);
